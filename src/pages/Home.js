8d9b09c16ffbeeab3009fd56c35bde0c899ba40c
DavidH <103000000+KuroneOuO@users.noreply.github.com>
2025-05-30 15:31:31 -0500

    Add files via upload

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Spotify Clone</h1>
+      <p>This is a simple React version of a Spotify-like UI.</p>
+    </div>
+  );
+};
+
+export default Home;
